11811ddcd9bacf802630f45c111dc653845e15bf
popples <klauspopp@gmx.de>
2025-08-29 09:56:27 +0200

    fix

---
+++ actions/run_in_devcontainer/run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# $1 = image
+# $2 = command
+
+image=$1
+command=$2
+
+docker run 
+    -u $(id -u):$(id -g) \
+    --rm \
+    -v $PWD:/workspace \
+    -w /workspace \
+    ${image} \
+    bash -c '\
+        groupadd -g "$(id -g)" hostgrp && \
+        useradd -M -s /bin/bash -u "$(id -u)" -g hostgrp hostusr && \
+        mkdir -p /home/hostusr && chown hostusr:hostgrp /home/hostusr && \
+        source /opt/esp/idf/export.sh && \
+        ${command}'
