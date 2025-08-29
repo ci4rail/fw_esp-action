1755d4c7385723cfb39aca0272e0af24da818dc4
popples <klauspopp@gmx.de>
2025-08-29 11:37:29 +0200

    fix

--- actions/run_in_devcontainer/run.sh
+++ actions/run_in_devcontainer/run.sh
@@ -4,4 +4,4 @@
 command=$1
 CONTAINER_NAME=esp-devc
 
-docker exec -u $(id -u):$(id -g) ${CONTAINER_NAME} bash -c "${command}"
+docker exec -u $(id -u):$(id -g) ${CONTAINER_NAME} bash -c "source /opt/esp/idf/export.sh && ${command}"
