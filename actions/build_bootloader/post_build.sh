94b8e92045e7b281fc478525757d66de02638ba6
popples <klauspopp@gmx.de>
2025-08-31 17:22:45 +0200

    fix

--- actions/build_bootloader/post_build.sh
+++ actions/build_bootloader/post_build.sh
@@ -1,38 +1,24 @@
 #!/usr/bin/env bash
 
 # Arguments: 
-# $1: build-dfu
-# $2: project-name
+# $1: project-name
 
-if [ -z "$2" ]; then
+if [ -z "$1" ]; then
   echo "Error: too few arguments"
   exit 1
 fi
 
-build_dfu=$1
-project_name=$2
-
-
-if [ "${build_dfu}" = "true" ]; then
-    idf.py dfu
-fi
+project_name=$1
 
 build_dir=$(find . -maxdepth 1 -type d -name 'build*' -printf '%f\n' | sort | head -n1)
 echo ${build_dir}
 
 version=$(awk 'match($0,/PROJECT_VER=\\"[\.a-z0-9\-]+\\"/) { print substr($0,RSTART+14,RLENGTH-16)}' ${build_dir}/build.ninja)
-app_file=${project_name}-${version}.bin
-app_file_path=${build_dir}/${app_file}
-cp ${build_dir}/${project_name}.bin ${app_file_path}
-
-dfu_file=""
-if [ "${build_dfu}" = "true" ]; then
-    dfu_file="${app_file_path}.dfu.bin"
-    cp ${build_dir}/dfu.bin ${dfu_file}
-fi
+bl_file=${project_name}-${version}.bin
+bl_file_path=${build_dir}/bootloader/${bl_file}
+cp ${build_dir}/bootloader/bootloader.bin ${bl_file_path}
 
 echo "build-dir=${build_dir}" >> $GITHUB_OUTPUT
-echo "app-file=${app_file}" >> $GITHUB_OUTPUT
-echo "app-file-path=${app_file_path}" >> $GITHUB_OUTPUT
+echo "bootloader-file=${bl_file}" >> $GITHUB_OUTPUT
+echo "bootloader-file-path=${bl_file_path}" >> $GITHUB_OUTPUT
 echo "version=${version}" >> $GITHUB_OUTPUT
-echo "dfu-file=${dfu_file}" >> $GITHUB_OUTPUT
